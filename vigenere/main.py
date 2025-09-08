963c65904153a2eb4824759af47493d263811fee
dtimb <dyl.timbrook@gmail.com>
2025-09-08 18:54:03 -0400

    solved 1

---
+++ vigenere/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from pwn import *
+
+context.proxy = (socks.SOCKS5, 'localhost', 8123) # SOCKS proxy
+host = '192.168.2.99' # Remote machine name
+port = 50494 # Remote port
+conn = remote(host, port)
+content = conn.recv(1024)
+print(content)
+
+conn.close()
+
